3814a9905d0a5e97991e10163c389cf1cbf455d6
JelleKomm1500 <32571630+JelleKomm1500@users.noreply.github.com>
2019-10-31 15:33:08 +0100

    Wip Controller

---
+++ MqttController/MqttController/Motorised.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MqttController
+{
+    class Motorised
+    {
+        private string mqttMessage;
+        private string topic;
+        private int direction;
+        //2d array with sensor directions
+        private string [,] trafficLightGroups =  
+        {
+            //North
+            {"/motorised/0/sensor/0", "/motorised/0/sensor/1", "/motorised/1/0/sensor/0", "/motorised/1/0/sensor/1", "/motorised/1/1/sensor/0", "/motorised/1/1/sensor/1", "/motorised/2/sensor/0", "/motorised/2/sensor/1"},
+            //East
+            {"/motorised/3/sensor/0", "/motorised/3/sensor/1", "/motorised/4/sensor/0", "/motorised/4/sensor/1" , "", "", "", ""},
+            //South
+            {"/motorised/5/0/sensor/0", "/motorised/5/0/sensor/1", "/motorised/5/1/sensor/0", "/motorised/5/1/sensor/1", "/motorised/6/sensor/0", "/motorised/6/sensor/1", "", ""},
+            //West
+            {"/motorised/7/sensor/0", "/motorised/7/sensor/1", "/motorised/8/sensor/0", "/motorised/8/sensor/1" , "", "", "", ""}
+        };
+
+        public Motorised(string _topic, string _mqttMessage)
+        {
+            this.mqttMessage = _mqttMessage;
+            this.topic = _topic;
+
+            //looks for bounds of 2d array
+            int bound0 = trafficLightGroups.GetUpperBound(0);
+            int bound1 = trafficLightGroups.GetUpperBound(1);
+
+            //checks 2d array if it matches the topic, returns value of the row, which is also the direction.
+            for (int i = 0; i <= bound0; i++)
+            {
+                for (int x = 0; x <= bound1; x++)
+                {
+                    if (topic.Contains(trafficLightGroups[i,x]) && trafficLightGroups[i, x] != "")
+                    {
+                        this.direction = i;
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ MqttController/MqttController/MqttPublish.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace MqttController
+{
+    class MqttPublish
+    {
+        private string team_id { get; set; }
+        private static string brokerAddress = "arankieskamp.com";
+
+        public void publish(string message, string topic)
+        {
+            // Create Client instance
+            MqttClient myClient = new MqttClient(brokerAddress);
+
+            byte code = myClient.Connect(Guid.NewGuid().ToString());
+
+            if (myClient.IsConnected == true)
+            {
+                Console.WriteLine("Connected to " + brokerAddress);
+            }
+
+            myClient.Publish(topic, Encoding.UTF8.GetBytes(message), MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE, false);
+            myClient.Disconnect();
+        }
+    }
+}
