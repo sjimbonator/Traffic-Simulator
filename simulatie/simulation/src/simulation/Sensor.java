d842a0f2edc6b86e6d69554d5298516586518acc
Kevin HBO-ICT <chen1700@student.nhl.nl>
2019-11-12 11:19:49 +0100

    multi threading with swingworker

--- simulatie/simulation/src/simulation/Sensor.java
+++ simulatie/simulation/src/simulation/Sensor.java
@@ -1,21 +1,23 @@
 package simulation;
 
 import java.util.ArrayList;
+import javax.swing.SwingWorker;
 
 public class Sensor {
 
     private double x;
     private double y;
-    private MqttPublisher publisher;
+    private String topic;
+    private MqttPublisher publisher = new MqttPublisher(topic);
     
     private int payload = 0;
 
     public Sensor(String topic, double x, double y) {
-        this.publisher = new MqttPublisher(topic);
         this.x = x;
         this.y = y;
+        this.topic = topic;
     }
-
+    
     public void update(ArrayList<WorldObject> worldObjects) {
         int tempPayload = 0;
         for (WorldObject object : worldObjects) {
@@ -25,10 +27,16 @@ public class Sensor {
                 tempPayload = 1;
             }
         }
-        
         if(tempPayload != payload){
             payload = tempPayload;
-            publisher.publish("" + payload);
+            SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
+            @Override
+            protected Void doInBackground() throws Exception {
+                publisher.publish("" + payload);
+                 return null;
+                }
+            };
+            worker.execute();
         }
     }
 }
