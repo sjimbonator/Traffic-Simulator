425f91e44d0ed3f194d2c6f421d5b540026eb724
JelleKomm1500 <32571630+JelleKomm1500@users.noreply.github.com>
2019-11-19 15:49:01 +0100

    Start Groepen

---
+++ MqttController/MqttController/GroupDecide.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MqttController
+{
+    class GroupDecide
+    {
+        private string mqttMessage;
+        private string topic;
+        private string team_id;
+
+        private string[,] trafficLightGroups =
+        {
+            //Group1 2x Noord naar Zuid en 1x Zuid naar Noord
+            {"/motorised/1/0/sensor/0", "/motorised/1/0/sensor/1", "/motorised/1/1/sensor/0", "/motorised/1/1/sensor/1", "/motorised/5/0/sensor/0", "/motorised/5/0/sensor/0", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",},
+            //Group2 Alle voetgangers sensoren + fietsers sensoren
+            {"/foot/0/0/sensor/0", "/foot/0/1/sensor/0", "/foot/1/0/sensor/0", "/foot/1/1/sensor/0", "/foot/2/sensor/0", "/foot/3/sensor/0", "/foot/4/0/sensor/0", "/foot/4/1/sensor/0", "/foot/4/2/sensor/0", "/foot/5/0/sensor/0", "/foot/5/1/sensor/0", "/foot/5/2/sensor/0", "/foot/6/sensor/0", "/foot/7/sensor/0", "cycle/0/sensor/0", "cycle/1/sensor/0", "cycle/2/sensor/0", "cycle/3/0/sensor/0", "cycle/3/1/sensor/0", "cycle/4/sensor/0", "cycle/5/sensor/0"},
+        };
+        public GroupDecide(string topic, string mqttMessage, string team_id)
+        {
+            this.mqttMessage = mqttMessage;
+            this.topic = topic;
+            this.team_id = team_id;
+        }
+    }
+}
